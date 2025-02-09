63166f3119297e6ada525b0aeb6ff20dde67d946
Mathias Thierbach <mt-dev@outlook.com>
2025-02-09 21:52:20 +0000

    code docs

--- src/navidataIO.Utils/AssemblyVersionInformation.cs
+++ src/navidataIO.Utils/AssemblyVersionInformation.cs
@@ -4,11 +4,29 @@ using System.Reflection;
 
 namespace navidataIO.Utils;
 
+/// <summary>
+/// Provides information about the version of an assembly.
+/// </summary>
 public class AssemblyVersionInformation
 {
+    /// <summary>
+    /// The consolidated version of the assembly. Returns the informational version if present, otherwise the file version, otherwise the assembly version.
+    /// </summary>
     public string Version { get; }
+
+    /// <summary>
+    /// The assembly version, if present.
+    /// </summary>
     public string? AssemblyVersion { get; }
+    
+    /// <summary>
+    /// The assembly file version, if present.
+    /// </summary>
     public string? FileVersion { get; }
+    
+    /// <summary>
+    /// The assembly informational version, if present.
+    /// </summary>
     public string? InformationalVersion { get; }
 
     private AssemblyVersionInformation(Assembly assembly)
@@ -25,9 +43,18 @@ public class AssemblyVersionInformation
         Version = InformationalVersion ?? FileVersion ?? AssemblyVersion ?? "Unknown";
     }
 
-    public static AssemblyVersionInformation FromAssembly(Assembly assembly) => new AssemblyVersionInformation(assembly);
+    /// <summary>
+    /// Creates an instance of <see cref="AssemblyVersionInformation"/> from the specified <paramref name="assembly"/>.
+    /// </summary>
+    public static AssemblyVersionInformation FromAssembly(Assembly assembly) => new AssemblyVersionInformation(assembly ?? throw new ArgumentNullException(nameof(assembly)));
 
+    /// <summary>
+    /// Creates an instance of <see cref="AssemblyVersionInformation"/> from the assembly of the calling method.
+    /// </summary>
     public static AssemblyVersionInformation FromCallingAssembly() => new AssemblyVersionInformation(Assembly.GetCallingAssembly());
 
+    /// <summary>
+    /// Creates an instance of <see cref="AssemblyVersionInformation"/> from the assembly of the specified <typeparamref name="T"/>.
+    /// </summary>
     public static AssemblyVersionInformation FromType<T>() => new AssemblyVersionInformation(typeof(T).Assembly);
 }
