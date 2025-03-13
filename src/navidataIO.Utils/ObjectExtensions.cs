eb9df678cef58a4ece250f6ff3176929e29f64e8
Mathias Thierbach <mt-dev@outlook.com>
2025-03-13 17:51:10 +0000

    object.With()

---
+++ src/navidataIO.Utils/ObjectExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace navidataIO.Utils;
+
+public static class ObjectExtensions
+{
+    public static T With<T>(this T target, Action<T>? action)
+    {
+        action?.Invoke(target ?? throw new ArgumentNullException(nameof(target)));
+        return target;
+    }
+}
