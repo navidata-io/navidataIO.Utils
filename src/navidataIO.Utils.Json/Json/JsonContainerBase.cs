922e6d4e13b3c38227096bb099642f39ebbfb7bb
Mathias Thierbach <mt-dev@outlook.com>
2025-03-19 21:40:30 +0000

    +JObjectBase

---
+++ src/navidataIO.Utils.Json/Json/JsonContainerBase.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace navidataIO.Utils.Json;
+
+/// <summary>
+/// Base class for a Json serializable type with support for unmapped properties.
+/// </summary>
+public abstract class JObjectBase
+{
+    [JsonExtensionData]
+    protected IDictionary<string, JToken?>? AdditionalProperties;
+}
