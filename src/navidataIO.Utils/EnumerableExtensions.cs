db44d1c8d1a4c2a5df3f3839bd79ee89baf72490
Mathias Thierbach <mt-dev@outlook.com>
2025-04-29 11:16:03 +0100

    IEnumerable<T>.WhereNotNull()

---
+++ src/navidataIO.Utils/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2024 navidata.io Corp
+// LICENSE-SPDX: <LGPL-3.0-only>
+
+namespace navidataIO.Utils;
+
+/// <summary>
+/// Provides LINQ extension methods.
+/// </summary>
+public static class EnumerableExtensions
+{
+    /// <summary>
+    /// Filters a collection of nullable values, passing through only the non-null values.
+    /// </summary>
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) => source.Where(x => x is not null)!;
+}
